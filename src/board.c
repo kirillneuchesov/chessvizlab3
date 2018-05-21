3e68a225e1802dff00d22bc5cc2ede7b36d8ba31
kirillneuchesov <36182439+kirillneuchesov@users.noreply.github.com>
2018-05-21 23:46:18 +0700

    updated board.c, board.h and combined it with board_print_plain.c, board_print_plain.h

--- src/board.c
+++ src/board.c
@@ -1,94 +1,101 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "board.h"
-void move ( char cell[9][9]){
-char hod[6];
-int o=0,p=0,i=0,o1=0,p1=0;
-int flag=1,error=0;
-char temp;
-do{
-error=0;
-scanf("%s",hod);
-for(i=0;i<5;i++){
-switch(hod[i])
+#include "board_read.h"
+
+char **arr;
+
+char **board()
 {
-    case 'a':
-    if (flag){
-	p=1;}else {p1=1;}
-	break;
-    case 'b':
-    if (flag){
-	p=2;}else {p1=2;}
-	break;
-    case 'c':
-    if (flag){
-	p=3;}else {p1=3;}
-	break;
-    case 'd':
-    if (flag){
-	p=4;}else {p1=4;}
-	break;
-    case 'e':
-    if (flag){
-	p=5;}else {p1=5;}
-	break;
-    case 'f':
-    if (flag){
-	p=6;}else {p1=6;}
-	break;
-    case 'g':
-    if (flag){
-	p=7;}else {p1=7;}
-	break;
-    case 'h':
-    if (flag){
-	p=8;}else {p1=8;}
-	break;
-    case '-':
-	flag=0;
-	break;
-    case '8':
-    if (flag){
-	o=0;}else {o1=0;}
-	break;
-    case '7':
-    if (flag){
-	o=1;}else {o1=1;}
-	break;
-    case '6':
-    if (flag){
-	o=2;}else {o1=2;}
-	break;
-    case '5':
-    if (flag){
-	o=3;}else {o1=3;}
-	break;
-    case '4':
-    if (flag){
-	o=4;}else {o1=4;}
-	break;
-    case '3':
-    if (flag){
-	o=5;}else {o1=5;}
-	break;
-    case '2':
-    if (flag){
-	o=6;}else {o1=6;}
-	break;
-    case '1':
-    if (flag){
-	o=7;}else {o1=7;}
-	break;
-    default:
-      error=1;
-      break;
+    arr =  (char**) malloc(8 * sizeof(char*));
+    int i,j;
+
+    for (i = 0; i < 8; i++) {
+    arr[i] = (char*)malloc(8 * sizeof(char));
+        for (j = 0; j < 8; j++) {
+            arr[i][j] = ' ';
+        }
+    }
+
+    char gl[]={'r','h','e','q','k','e','h','r'};
+    char p ='p';
+
+    for (i=0; i< 8; i++) {
+        arr[0][i] = gl[i];
+        arr[7][i] = gl[i] - 32;
+        arr[1][i] = p;
+        arr[6][i] = p - 32;
+    }    
+    return arr; 
 }
+
+void print_new_board()
+{
+    arr = board();
+    print_board(arr);
 }
-if (error){
-printf("\nEnter the correct data\n");}
-}while (error);
-temp=cell[o][p];
-cell[o][p]=' ';
-cell[o1][p1]=temp;
-printf("\n\n");
+
+void print_board(char **arr)
+{
+	int i,j;
+	char p;
+	system("cls");
+    printf("\n"); 
+    for (i = 0; i < 8; i++) {
+        printf("\t %d", 8 - i);
+        for (j = 0; j < 8; j++) {
+            printf(" %c", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\t  ");
+    
+    for (p = 'a'; p <= 'h'; p++) {
+        printf(" %c", p);
+    }
+    
+    printf("\n");
+}
+
+char** movePawn(char **v, int* pozition)
+{
+    int poz = arr[pozition[1]][pozition[0]];
+    arr[pozition[1]][pozition[0]] = arr[pozition[3]][pozition[2]];
+    arr[pozition[3]][pozition[2]] = poz;
+   
+    return v;
+}
+
+
+int board_func(char *places, int test) 
+{
+	int g;
+    if (strlen(places) != 5) {
+        return -1;
+    }
+    int pozition[4];
+    pozition[0] = places[0] - 96 - 1; //a
+    pozition[1] = 9 - (places[1] - 48) - 1; //2
+    pozition[2] = places[3] - 96 - 1; //c
+    pozition[3] = 9 - (places[4] - 48) - 1; //8
+    printf("\n ");
+    
+    int check = move_figure(pozition, arr);
+    if (check == -1) { 
+		return -1; 
+	}
+
+    for(g = 0; g < 4; g++) {
+        if (pozition[g] < 0 || pozition[g] > 8) {
+            return -1;
+        }
+    }
+    arr = movePawn(arr, pozition);
+    if (test == 0) {
+        print_board(arr);
+    }
+    return 0;
 }
--- src/board.h
+++ src/board.h
@@ -1,1 +1,13 @@
-void move ( char cell[9][9]);
+#ifndef _CHESS_
+#define _CHESS_
+
+    void print_new_board();
+
+    int board_func(char *places, int test);
+
+    void print_board(char **arr);
+
+    char **board();
+    
+
+#endif
